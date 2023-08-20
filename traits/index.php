35e7de20fbc5f1d56c2ed6a50a30664fd4555806
Iffat Jahan <iffatjahan046@gmail.com>
2023-08-20 23:19:51 +0600

    traits

--- traits/index.php
+++ traits/index.php
@@ -1,1 +1,29 @@
 <?php
+
+trait science{
+    public function mathematics()
+    {
+        return "My subject is mathematics";
+    }
+}
+
+trait commerce{
+    public function accounting()
+    {
+        return "My subject is accounting";
+    }
+}
+trait arts{
+    public function English()
+    {
+        return "My subject is English";
+    }
+}
+class allgroups{
+    use science,commerce,arts;
+}
+
+$mysubject=new allgroups();
+echo $mysubject->English();
+echo $mysubject->accounting();
+echo $mysubject->mathematics();
